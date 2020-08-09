7aa628e5d19e616702815fc6730079373304b4aa
David Schweikert <david@schweikert.ch>
2020-08-09 16:36:26 +0200

    improve grafana startup script

--- test/grafana-start.sh
+++ test/grafana-start.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+password=$(pwgen -1)
+url=http://portia.schweikert.ch:3000
+
 if [ "$1" != "stop" ]; then
-docker run \
-  -d \
-  --rm \
-  --net host \
-  --name=grafana \
-  -e "GF_SERVER_ROOT_URL=http://portia.schweikert.ch:3000" \
-  -e "GF_SECURITY_ADMIN_PASSWORD=lou6iser" \
-  grafana/grafana
+    docker run \
+      -d \
+      --rm \
+      --net host \
+      --name=grafana \
+      -e "GF_SERVER_ROOT_URL=$url" \
+      -e "GF_SECURITY_ADMIN_PASSWORD=$password" \
+      grafana/grafana
+
+    echo "grafana started: $url"
+    echo "admin password: $password"
+    echo
 else
-docker stop grafana
+    docker stop grafana
 fi
